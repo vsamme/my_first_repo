4c713dfe9dc6956fa593d1424b309c96b8709e52
vsamme <58043997+vsamme@users.noreply.github.com>
2019-11-21 23:25:08 +0530

    Updated code

--- guessinggame.sh
+++ guessinggame.sh
@@ -1,1 +1,21 @@
-echo "Welocme"
+echo "Welcome to guess_numbering Game"
+echo "Enter your guess_numbe: "
+read guess_number
+function get_files {
+	local number=$(ls -l | wc -l)-1
+	echo $number
+}
+correct_number=$(get_files)
+while [[ $guess_number -ne $correct_number ]]
+do
+	if [[ $guess_number -gt $correct_number ]]
+	then
+		echo "Sorry, your guess_number was too high"
+	else 
+		echo "Sorry, your guess_number was too low"
+	fi
+	echo
+	echo "Sorry, Try again: "
+	read guess_number_number
+done
+echo "Congratulations! You guess the right number!"
